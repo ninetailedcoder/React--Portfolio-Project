6f7cb3f74131b9c75015df5a1fd1cea9df56eb44
Tory DeMaio <torydemaio@gmail.com>
2023-02-15 12:36:02 -0600

    added images to img folder

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import { Col,Row,Container, Form } from "reactstrap";
+import f
+
+const Header = () =>{
+    return(
+        <Container>
+            <Row>
+                <Col className="text-center" md='3'>
+                    <Link to='/'><img ></img></Link>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+export default Header
